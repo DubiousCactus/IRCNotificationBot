e5d99aaefd81d5c66a8fe724012e84051453b55a
Theo Morales <theo.morales.fr@gmail.com>
2018-02-28 17:30:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,20 +6,40 @@
 # Distributed under terms of the MIT license.
 #
 
+if [ "$(whoami)" == "root" ]; then
+   	echo "[!] Don't run this installer as root you fool !"
+   	exit 1;
+fi
+
+echo "[*] Creating ~/.config/IRCNotificationBot"
 mkdir -p ~/.config/IRCNotificationBot/
 cd ~/.config/IRCNotificationBot
-wget https://raw.githubusercontent.com/M4gicT0/IRCNotificationBot/master/config.json
+echo "[*] Downloading config.json from GitHub"
+wget --quiet https://raw.githubusercontent.com/M4gicT0/IRCNotificationBot/master/config.json
 
+echo "[*] Downloading systemd service file from GitHub"
+wget --quiet https://raw.githubusercontent.com/M4gicT0/IRCNotificationBot/master/IRCNotificationBot.service
+echo "[*] Setting the User parameter of the service"
+# Set User=$(whoami) in the service file (so that it doesn't run as root and can access the user's home dir)
+sed "s/##USER##/$(whoami)/g" IRCNotificationBot.service > IRCNotificationBot.service.tmp && mv IRCNotificationBot.service.tmp IRCNotificationBot.service
+
+echo "[*] Asking for root priviledges to enable the service"
+# Become root if not already
 [ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
 
+mv IRCNotificationBot.service /usr/lib/systemd/system/
+
+echo "[*] Creating /opt/IRCNotificationBot"
 mkdir /opt/IRCNotificationBot
 cd /opt/IRCNotificationBot
 
-wget https://raw.githubusercontent.com/M4gicT0/IRCNotificationBot/master/watchdog.py
-wget https://raw.githubusercontent.com/M4gicT0/IRCNotificationBot/master/IRCNotificationBot.service
+echo "[*] Downloading the service script from GitHub"
+wget --quiet https://raw.githubusercontent.com/M4gicT0/IRCNotificationBot/master/watchdog.py
 
+echo "[*] Giving execution rights to the script"
 chmod +x watchdog.py
-sed -i.bak "s/##USER##/$(whoami)/g" IRCNotificationBot.service
-mv IRCNotificationBot.service /usr/lib/systemd/system/
+
+echo "[*] Enabling the systemd service"
+# Enable the systemd service
 systemctl enable IRCNotificationBot.service
 systemctl start IRCNotificationBot.service
