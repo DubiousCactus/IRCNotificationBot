64058786022716729a25967c92ef1033217c7324
Theo Morales <theo.morales.fr@gmail.com>
2018-02-28 12:23:43 +0100

    Fix install.sh and systemd service

--- install.sh
+++ install.sh
@@ -7,9 +7,14 @@
 #
 
 
-mkdir ~/.irc_notifier && cd ~/.irc_notifier
-wget https://raw.githubusercontent.com/M4gicT0/IRCNotificationBot/master/watchdog.py .
-wget https://raw.githubusercontent.com/M4gicT0/IRCNotificationBot/master/irc_notifier.service
-mv irc_notifier.service /etc/sysctl.d/
-systemctl enable irc_notifier.service
+[ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
 
+mkdir /opt/irc_notifier
+cd /opt/irc_notifier
+
+wget https://raw.githubusercontent.com/M4gicT0/IRCNotificationBot/master/watchdog.py
+wget https://raw.githubusercontent.com/M4gicT0/IRCNotificationBot/master/irc_notifier.service
+
+mv irc_notifier.service /etc/systemd/system/
+systemctl enable irc_notifier.service
+systemctl start irc_notifier.service
